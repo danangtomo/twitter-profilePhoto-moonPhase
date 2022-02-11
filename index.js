1fd989406d2f148998ec24c2feff7b19ae08f244
Danang Estutomoaji <ajidanang9@gmail.com>
2022-02-11 14:15:56 +0700

    temp

--- index.js
+++ index.js
@@ -99,5 +99,5 @@ app.get('/', (req, res) => {
  
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`App listening on port ${port}`);
 });
