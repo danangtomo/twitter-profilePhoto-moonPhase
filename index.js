aa3b2d93e49073ffd32dda55525892a0c702e548
Danang Estutomoaji <ajidanang9@gmail.com>
2022-02-21 04:22:20 +0700

    passed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import { ApiUrl } from './const/index.js';
 
 const app = express();
 
-const midnight = "04:20:00";
+const midnight = "00:00:10";
 let getLocalDateData = moment.utc().tz("Asia/Jakarta");
 let today = getLocalDateData.format('YYYY-MM-DD');
 
